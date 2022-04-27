98a250963337b2dee0f3f539e1e306828bd8ab21
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-04-27 21:29:27 +0900

    こちらがVer.1.3

--- Shared/StopWatchClass.swift
+++ Shared/StopWatchClass.swift
@@ -28,8 +28,8 @@ class StopWatchManeger:ObservableObject{
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true){ timer in
             
             self.elapsedTime = NSDate.timeIntervalSinceReferenceDate
-            self.displayTime = ((self.elapsedTime + self.savedTime) - self.nowTime) * 600
-//            self.displayTime = (self.elapsedTime + self.savedTime) - self.nowTime
+//            self.displayTime = ((self.elapsedTime + self.savedTime) - self.nowTime) * 600
+            self.displayTime = (self.elapsedTime + self.savedTime) - self.nowTime
             
             // ミリ秒は小数点第一位、第二位なので100をかけて100で割った余り
             self.milliSecond = Int(self.displayTime * 100) % 100
@@ -83,8 +83,8 @@ class StopWatchManeger2:ObservableObject{
         timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true){ timer in
             
             self.elapsedTime = NSDate.timeIntervalSinceReferenceDate
-            self.displayTime = ((self.elapsedTime + self.savedTime) - self.nowTime) * 600
-//            self.displayTime = (self.elapsedTime + self.savedTime) - self.nowTime
+//            self.displayTime = ((self.elapsedTime + self.savedTime) - self.nowTime) * 600
+            self.displayTime = (self.elapsedTime + self.savedTime) - self.nowTime
             
             // ミリ秒は小数点第一位、第二位なので100をかけて100で割った余り
             self.milliSecond = Int(self.displayTime * 100) % 100
